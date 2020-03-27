e5bf813cd1ab650034a468081398c96ed7415974
tibike8906 <47151702+tibike8906@users.noreply.github.com>
2020-03-27 09:21:38 +0100

    Init

---
+++ EightQueen/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace EightQueen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private int[,] _table;
+        private int _step = 0;        
+
+        private List<Image> _eightQueen;
+
+        private QueenAlgorythm _queen_alg;
+        private BitmapImage _queen_bitmap = new BitmapImage(new Uri("q1.png", UriKind.RelativeOrAbsolute));
+        private DispatcherTimer _timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _eightQueen = new List<Image>();
+            for (int i = 0; i < 8; i++)
+            {
+                
+                Image img = new Image();
+                img.Width = 80;
+                img.Height = 80;
+                img.Source = _queen_bitmap;
+                img.Name = "q"+ i;
+                _eightQueen.Add(img);
+                grWaiterZone.Children.Add(img);                
+            }
+            _timer = new DispatcherTimer();
+            _timer.Tick += timer_Tick;
+
+        }             
+        void timer_Tick(object sender, EventArgs e)
+        {
+
+            _timer.Interval = TimeSpan.FromMilliseconds(1000 - sSpeed.Value);
+            
+            if (_step >= _queen_alg.GetSteps().Count())
+            {
+                _timer.Stop();
+                if (_eightQueen.Where(p => p.Parent == grWaiterZone).Count() > 0)                
+                    MessageBox.Show("A megadott kezdőhellyel az algoritmus sajnos nem talált megoldást!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                else
+                    MessageBox.Show("Az algoritmus sikeres megoldást talált!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);                
+                btnRemoveTable.IsEnabled = true;
+                return;
+            }
+            
+            _table = _queen_alg.GetSteps()[_step];            
+            RemoveQueenFromChessTable();
+            ConvertTable(_table);
+            _step++;
+        }
+        private void AddQueenToChessTable(int row, int column)
+        {
+            Image q = _eightQueen.Where(p => p.Parent == grWaiterZone).First();
+            grWaiterZone.Children.Remove(q);
+            grChessTable.Children.Add(q);
+            Grid.SetRow(q, row);
+            Grid.SetColumn(q, column);
+        }
+
+        private void RemoveQueenFromChessTable()
+        {
+            foreach (var item in _eightQueen.Where(p => p.Parent == grChessTable))
+            {
+                item.Source = _queen_bitmap;
+                grChessTable.Children.Remove(item);
+                grWaiterZone.Children.Add(item);
+            }
+        }
+
+        private void ConvertTable(int[,] table)
+        {
+            for (int i = 0; i < 8; i++)            
+                for (int j = 0; j < 8; j++)
+                    if (table[i, j] == 1)                                            
+                        AddQueenToChessTable(i, j);                                                                        
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int[] pos = new int[2];
+            pos = QueenAlgorythm.GetNumPosition(tbFirstQueenPosition.Text);
+            
+            if (tbFirstQueenPosition.Text == string.Empty)
+            {
+                Random rnd = new Random();
+                pos[0] = rnd.Next(0, 7);
+                pos[1] = rnd.Next(0, 7);
+                MessageBox.Show($"Nem adott meg kezdő helyet!\nRandom hely: {QueenAlgorythm.GetAlpPostion(pos)}","Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else 
+            {                
+                bool isValid = pos[0] != 99 && pos[1] != 99;
+                if (!isValid) 
+                {
+                    MessageBox.Show($"Érvénytelen helyet adott meg!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            tbFirstQueenPosition.IsEnabled = false;
+            btnStart.IsEnabled = false;
+
+            _queen_alg = new QueenAlgorythm(pos[0], pos[1]);
+            _queen_alg.Solve();
+            _timer.Interval = TimeSpan.FromMilliseconds(1000 - sSpeed.Value);
+            _timer.Start();
+
+        }
+
+        private void btnRemoveTable_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveQueenFromChessTable();
+            _step = 0;
+            _queen_alg = null;
+            tbFirstQueenPosition.Text = "";
+            tbFirstQueenPosition.IsEnabled = true;
+            btnStart.IsEnabled = true;
+            btnRemoveTable.IsEnabled = false;
+
+        }
+    }
+}
---
+++ EightQueen/QueenAlgorythm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EightQueen
+{
+
+    class QueenAlgorythm
+    {
+        private static string[] _EnabledAlpa = { "A", "B", "C", "D", "E", "F", "G", "H" };
+        private static string[] _EnabledNum = { "1", "2", "3", "4", "5", "6", "7", "8" };
+
+        private List<int[,]> _steps_of_table;
+        private int[,] _table;
+
+        private const int _size_of_table = 8;
+        
+        private int _start_row, _start_column;
+                                        
+        public QueenAlgorythm(int StartRow, int StartColumn)
+        {
+            _start_row = StartRow;
+            _start_column = StartColumn;            
+            _table = new int[_size_of_table, _size_of_table];
+            FillTable(_start_row, _start_column);
+            _steps_of_table = new List<int[,]>();
+        }
+        public List<int[,]> GetSteps() { return _steps_of_table; }
+        public static int[] GetNumPosition(string postion)
+        {
+            int[] pos = new int[2];
+            pos[0] = 99;
+            pos[1] = 99;
+
+
+            if (postion.Length != 2) return pos;
+            if (!_EnabledAlpa.Contains(postion.Substring(0, 1)) || !_EnabledNum.Contains(postion.Substring(1, 1))) return pos;
+            for (int i = 0; i < _EnabledAlpa.Length; i++)
+            {
+                if (_EnabledAlpa[i] == postion.Substring(0, 1))
+                {
+                    pos[0] = i;
+                    break;
+                }
+            }
+            pos[1] = Int32.Parse(postion.Substring(1, 1)) - 1;
+            return pos;
+        }
+        public static string GetAlpPostion(int[] position)
+        {
+            return _EnabledAlpa[position[0]] + _EnabledNum[position[1]].ToString();
+        }
+
+        public void Solve()
+        {             
+            bool s = SolveNQ(_start_column);
+        }
+        private bool SolveNQ(int Column)
+        {
+            if (Column >= _size_of_table)
+                return true;
+
+
+            
+            for (int i = 0; i < _size_of_table; ++i)
+            {
+                if (IsSafe(i, Column, _table))
+                {                                                            
+                    _table[i, Column] = 1;                    
+                    _steps_of_table.Add(CopyTable());
+                    if (SolveNQ(Column + 1))
+                        return true;
+
+                    _table[i, Column] = 0;                                        
+                }
+            }
+            
+            return false;
+        }
+        private int[,] CopyTable()
+        {
+            int[,] c = new int[_size_of_table, _size_of_table];
+            for (int i = 0; i < _size_of_table; i++)
+            {
+                for (int j = 0; j < _size_of_table; j++)
+                {
+                    c[i, j] = _table[i, j];
+                }
+            }
+            return c;
+        }
+        public bool IsSafe(int row, int column, int[,] table)
+        {
+            int i, j;
+            for (i = 0; i < column; ++i)
+                if (Convert.ToBoolean(table[row, i]))
+                    return false;
+
+            for (i = row, j = column; i >= 0 && j >= 0; --i, --j)
+                if (Convert.ToBoolean(table[i, j]))
+                    return false;
+
+            for (i = row, j = column; j >= 0 && i < _size_of_table; ++i, --j)
+                if (Convert.ToBoolean(table[i, j]))
+                    return false;
+            return true;
+        }
+        private void FillTable(int StartRow, int StartColumn)
+        {
+            for (int i = 0; i < _size_of_table; i++)
+            {
+                for (int j = 0; j < _size_of_table; j++)
+                {
+                    if (i == StartRow && j == StartColumn)
+                        _table[i, j] = 1;
+                    else
+                        _table[i, j] = 0;
+                }
+            }
+        }
+    }
+}
